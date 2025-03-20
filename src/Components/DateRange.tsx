f01d435f2935f0fc88241c37598f439902aa56a1
Sarah Prando <sarahprando40@gmail.com>
2025-03-20 07:33:23 -0300

    update overview  vendas/recebidos/processando

--- src/Components/DateRange.tsx
+++ src/Components/DateRange.tsx
@@ -1,9 +1,8 @@
-import React from "react";
 import DateInput from "./DateInput";
+import { useData } from "../Context/DataContext";
 
 const DateRange = () => {
-  const [inicio, setInicio] = React.useState("");
-  const [final, setFinal] = React.useState("");
+  const { inicio, setInicio, final, setFinal } = useData();
 
   return (
     <form className="box flex" onSubmit={(e) => e.preventDefault()}>
@@ -12,13 +11,11 @@ const DateRange = () => {
         value={inicio}
         onChange={({ target }) => setInicio(target.value)}
       />
-      {inicio}
       <DateInput
         label="Final"
         value={final}
         onChange={({ target }) => setFinal(target.value)}
       />
-      {final}
     </form>
   );
 };
