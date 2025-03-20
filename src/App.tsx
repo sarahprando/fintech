2750c6f6cc2d49e9bcf278cd64070378ac44d0fc
Sarah Prando <sarahprando40@gmail.com>
2025-03-20 07:33:23 -0300

    update overview  vendas/recebidos/processando

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import Header from "./Components/Header";
+import Sidenav from "./Components/Sidenav";
+import { DataContextProvider } from "./Context/DataContext";
+import Resumo from "./Pages/Resumo";
+import "./Style.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <DataContextProvider>
+      <div className="container">
+        <Sidenav />
+        <main>
+          <Header />
+          <Resumo />
+        </main>
+      </div>
+    </DataContextProvider>
+  );
 }
 
 export default App;
