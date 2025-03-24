29059f53af84f488656f0c397ec328bedf2aa23d
Sarah Prando <sarahprando40@gmail.com>
2025-03-24 06:13:11 -0300

    update vendas | sidenav | router | venda

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,33 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./Components/Header";
 import Sidenav from "./Components/Sidenav";
 import { DataContextProvider } from "./Context/DataContext";
 import Resumo from "./Pages/Resumo";
 import Vendas from "./Pages/Vendas";
 import "./Style.css";
+import Venda from "./Pages/Venda";
 
 function App() {
   return (
-    <DataContextProvider>
-      <div className="container">
-        <Sidenav />
-        <main>
-          <Header />
-          <Resumo />
-          <Vendas />
-        </main>
-      </div>
-    </DataContextProvider>
+    <BrowserRouter>
+      <DataContextProvider>
+        <div className="container">
+          <Sidenav />
+          <main>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Resumo />} />
+            </Routes>
+            <Routes>
+              <Route path="/vendas" element={<Vendas />} />
+            </Routes>
+            <Routes>
+              <Route path="/vendas/:id" element={<Venda />} />
+            </Routes>
+          </main>
+        </div>
+      </DataContextProvider>
+    </BrowserRouter>
   );
 }
 
